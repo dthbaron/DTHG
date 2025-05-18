d16256477959f57349a96a4cfc3d9bb2d9ecb025
Leo <leo@leos-macbook-pro-3.home>
2025-05-18 15:07:49 +0200

    general fixes

---
+++ index.js
@@ -0,0 +1,13 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+      page.style.display = page.id === pageId ? 'block' : 'none';
+    });
+    history.replaceState(null, null, '#' + pageId);
+  }
+
+  window.addEventListener('load', () => {
+    let hash = location.hash.replace('#', '');
+    if (!hash) hash = 'who-we-are';
+    showPage(hash);
+  });
