1825c3068b210faeb4197f641df0bf9ef22c225f
Leopold Kaniewski <leopoldmiszcz@outlook.com>
2026-02-02 14:28:03 +0100

    Big fixes

--- index.js
+++ index.js
@@ -11,3 +11,15 @@
 //     if (!hash) hash = 'who-we-are';
 //     showPage(hash);
 //   });
+
+function scrollToTargetAdjusted() {
+  var element = document.getElementById("targetElement");
+  var headerOffset = 45;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+}
